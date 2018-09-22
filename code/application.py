728488a41eac292aa8c38a523e9c7c3697df0fdd
Lucas Meine <lucas.meine@citara.io>
2018-09-22 19:35:56 -0300

    Devmind commit in 2018-09-22 19:35:55

--- code/application.py
+++ code/application.py
@@ -77,7 +77,7 @@ def show_recipes():
        
        
         templist = []
-        for i in range(0,1):
+        for i in range(0,3):
             
             cur.execute("SELECT name, cuisine, provider, big_image, ingredient_amount FROM recipe_info WHERE nutrition = %s and dbscan_label = %s;", [group[i], cur_flavor])
             temp = cur.fetchall()
