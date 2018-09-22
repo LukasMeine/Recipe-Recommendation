a0368975313d04e40f99012f28cbdc65e50d6cfc
Lucas Meine <lucas.meine@citara.io>
2018-09-22 17:21:15 -0300

    Devmind commit in 2018-09-22 17:21:14

--- code/application.py
+++ code/application.py
@@ -71,6 +71,7 @@ def show_recipes():
     entries = []
     count = 0
     for group in satisfied_recipes:
+        print group[i]
         templist = []
         for i in range(0,3):
             cur.execute("SELECT name, cuisine, provider, big_image, ingredient_amount FROM recipe_info WHERE  dbscan_label = %s;", [group[i], cur_flavor])
