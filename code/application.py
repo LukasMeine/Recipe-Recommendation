7f755a8b9dc89d0869a7c6ea53d6a1c89bbd1100
Lucas Meine <lucas.meine@citara.io>
2018-09-22 19:10:58 -0300

    Devmind commit in 2018-09-22 19:10:57

--- code/application.py
+++ code/application.py
@@ -62,7 +62,7 @@ def show_recipes():
     conn = mysql.connect()
     cur = conn.cursor()
     print cur_flavor
-    cur.execute("SELECT nutrition FROM recipe_info WHERE dbscan_label = %s;", [cur_flavor])
+    cur.execute("SELECT index, nutrition FROM recipe_info WHERE dbscan_label = %s;", [cur_flavor])
     fetch_result = cur.fetchall()
     satisfied_recipes = constraint.nutritional_constraints(fetch_result, cur_age, cur_weight, cur_height, cur_gender, 'Active')
     
@@ -79,7 +79,7 @@ def show_recipes():
         templist = []
         for i in range(0,3):
             
-            cur.execute("SELECT name, cuisine, provider, big_image, ingredient_amount FROM recipe_info WHERE nutrition = %s and dbscan_label = %s;", [group[i], cur_flavor])
+            cur.execute("SELECT name, cuisine, provider, big_image, ingredient_amount FROM recipe_info WHERE index = %s and dbscan_label = %s;", [group[i], cur_flavor])
             temp = cur.fetchall()
             #print temp
             templist.append(temp)
