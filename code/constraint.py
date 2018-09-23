e3288e6cccbef7875bde28ffd51edbb5cacef8e3
Lucas Meine <lucas.meine@citara.io>
2018-09-22 21:48:47 -0300

    Devmind commit in 2018-09-22 21:48:47

--- code/constraint.py
+++ code/constraint.py
@@ -39,12 +39,12 @@ def nutritional_constraints(yum, age, weight, height, gender, activity_level):
     n_nutrition= len(nutrition_list)
     for recipe in yum:
         my_nutrition=[0 for i in range(n_nutrition)]
-        temp_list = ast.literal_eval(recipe[0]) # convert unicode to python list of dictionary
+        temp_list = ast.literal_eval(recipe[1]) # convert unicode to python list of dictionary
         for element in temp_list:
             for i in range(n_nutrition):
                 if element['attribute'] == nutrition_list[i]:
                     my_nutrition[i] = element['value']*unit_ratio[i] #/recipe[14]
-        yum_list.append([recipe[1], my_nutrition])
+        yum_list.append([recipe[0], my_nutrition])
     N=len(yum_list)
     satisfied_list=[]
     for i in range(N):
